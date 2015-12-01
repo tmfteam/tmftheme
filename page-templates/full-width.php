03cf391f59d9f65f159758310eaf33061402e739
The Modern FrameWork <trevtunechi@gmail.com>
2015-12-01 18:41:49 +0300

    Create full-width.php

---
+++ page-templates/full-width.php
@@ -0,0 +1,40 @@
+<?php
+/**
+ * Template Name: Full Width Page
+ *
+ * @package TMF
+ * @since TMF 1.0
+ */
+
+get_header(); ?>
+
+<div id="main-content" class="main-content">
+
+<?php
+	if ( is_front_page() && $tmf->has_featured_posts() ) {
+		// Include the featured content template.
+		get_template_part( 'featured-content' );
+	}
+?>
+
+	<div id="primary" class="content-area">
+		<div id="content" class="site-content" role="main">
+			<?php
+				// Start the Loop.
+				while ( have_posts() ) : the_post();
+
+					// Include the page content template.
+					get_template_part( 'template-parts/content', 'page' );
+
+					// If comments are open or we have at least one comment, load up the comment template.
+					if ( comments_open() || get_comments_number() ) {
+						comments_template();
+					}
+				endwhile;
+			?>
+		</div><!-- #content -->
+	</div><!-- #primary -->
+</div><!-- #main-content -->
+
+<?php
+get_footer();
